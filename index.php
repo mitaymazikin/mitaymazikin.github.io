8a3bd7dbdb706fd719005172ce6d6a25b819a138
d.mazikin <mazikindima@mail.ru>
2020-01-13 10:23:03 +0300

    Переделываю форму

--- index.php
+++ index.php
@@ -1,86 +1,73 @@
 <!doctype html>
-<html lang="Ru">
+<html lang="Ru" xmlns="http://www.w3.org/1999/html">
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
+    <!-- Required meta tags -->
+    <meta charset="<?php mb_internal_encoding("UTF-8"); ?>>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="/css/jquery-ui.css">
-  <link rel="stylesheet" href="/css/style.css">
-  <!-- Bootstrap CSS-->
-  <title>Форма обратной связи</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/jquery-ui.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <!-- Bootstrap CSS-->
+    <title>Форма обратной связи</title>
 </head>
 <body>
-  <div class="container">
+<div class="container">
 
-    <form action="php/check.php" id="toForm" method="POST" enctype="multipart/form-data">
-      <h1 class="col-sm-12 text-center">Заполните форму, пожалуйста!</h1>
-      <div class="errors"></div>
-      <div class="form-group row">
-        <label  class="col-sm-2 col-form-label">Введите ФИО:</label>
-        <div class="col-sm-4">
-          <input name="fio" type="text" class="form-control" id="inputFio"  placeholder="Введите ФИО" >
+    <form id="toForm" method="POST" name="check" enctype="multipart/form-data">
+        <h1 class="col-sm-12 text-center">Заполните форму, пожалуйста!</h1>
+        <div class="errors"></div>
+        <div class="form-group row">
+            <label  class="col-sm-2 col-form-label">Введите ФИО:</label>
+            <div class="col-sm-4">
+                <input name="fio" type="text" class="form-control" id="inputFio"  placeholder="Введите ФИО" >
+            </div>
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label">Номер телефона</label>
-        <div class="col-sm-4">
-          <input name="phone" type="tel" class="form-control" id="inputPhone" >
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Номер телефона</label>
+            <div class="col-sm-4">
+                <input name="phone" type="tel" class="form-control" id="inputPhone" >
+            </div>
         </div>
-      </div>
 
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label">Email</label>
-        <div class="col-sm-4">
-          <input name="email" type="email" class="form-control" id="inpuEmail" placeholder="Email">
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-4">
+                <input name="email" type="email" class="form-control" id="inpuEmail" placeholder="Email">
+            </div>
         </div>
-      </div>
-      <div class="form-group">
-        <label >Загрузите файл:</label>
-        <input type="hidden" name="MAX_FILE_SIZE" value="50000" />
-        <input name="file" type="file" class="form-control-file" id="files">
-      </div>
-      <div class="form-group row">
-        <label  class="col-sm-2 col-form-label">Дата:</label>
-        <div class="col-sm-4">
-          <input name="date" type="text" class="form-control" id="inputDate">
+        <div class="form-group">
+            <label >Загрузите файл:</label>
+            <!-- <input type="hidden" name="MAX_FILE_SIZE" value="50000" /> -->
+            <input name="file" type="file" class="form-control-file" id="files">
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label">Количество:</label>
-        <div class="col-sm-4">
-          
-          <input type="text" name="range" id="amount" readonly >
-            <div id="sliderRange"></div>
+        <div class="form-group row">
+            <label  class="col-sm-2 col-form-label">Дата:</label>
+            <div class="col-sm-4">
+                <input name="date" type="text" class="form-control" id="inputDate">
+            </div>
         </div>
-      </div>
-      <div class="form-group row">
-      <!-- рекаптча -->
-        <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response" />
-          </div>
-      <div class="form-group row">
-        <div class="col-sm-12 ">
-          <button type="submit" id="submit" name="check_submit" class="btn btn-success">Отправить</button>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Количество:</label>
+            <div class="col-sm-4">
+                <div id="slider">
+                    <div id="custom-handle" class="ui-slider-handle" name="range"></div>
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="form-group row">
+            <div class="col-sm-12 ">
+                <button type="submit" id="submit" name="check_submit" class="btn btn-success">Отправить</button>
+            </div>
+        </div>
     </form>
 
-  </div>  
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script type="text/javascript" src="/js/jquery-3.4.1.min.js"></script>
-  <script type="text/javascript" src="/js/jquery.maskedinput.min.js"></script>
-  <script type="text/javascript" src="/js/jquery-ui.js"></script>
-  <script src="/js/main.js"></script>
-  <script src="https://www.google.com/recaptcha/api.js?render=6LfQB7kUAAAAABsyEVki2K2yYHZG7MBwNz7izDUw"></script>
-  <script>
-        grecaptcha.ready(function() {
-            grecaptcha.execute('6LfQB7kUAAAAABsyEVki2K2yYHZG7MBwNz7izDUw', {action: 'homepage'}).then(function(token) {
-                //console.log(token);
-                document.getElementById('g-recaptcha-response').value=token;
-            });
-        });
-    </script>
-  
+</div>
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script type="text/javascript" src="/js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="/js/jquery.maskedinput.min.js"></script>
+<script type="text/javascript" src="/js/jquery-ui.js"></script>
+<script src="/js/main.js"></script>
+<script src="/js/filecheck.js"></script>
 </body>
 </html>
--- php/check.php
+++ php/check.php
@@ -1,144 +1,39 @@
 <?php
-header('Content-Type: text/html; charset=UTF-8');
+$fio = htmlspecialchars($_POST['fio']);
+$phone = htmlspecialchars($_POST['phone']);
+$email = htmlspecialchars($_POST['email']);
+$date = htmlspecialchars($_POST['date']);
+$range = htmlspecialchars($_POST['range']);
+$errors = array();
 
-define('SITE_KEY', '6LfQB7kUAAAAABsyEVki2K2yYHZG7MBwNz7izDUw');
-define('SECRET_KEY', '6LfQB7kUAAAAAIR7POfnIW4WAZeNbgqdd_rgNQy0');
 
-function getCaptcha($SecretKey) {
-        $Response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".SECRET_KEY."&response={$SecretKey}");
-        $Return = json_decode($Response);
-        return $Return;
-    }
+//$_FILES
+//$_POST
 
-if ($_POST) {
-    $fio = $_POST['fio'];
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $date = $_POST['date'];
-    $range = $_POST['range'];
-    $errors = array();
+echo json_encode($_POST);
 
-    //Сообщение на почту
-    $msgEmail = nl2br("Данные сообщения:\r\n", false);
-    $msgEmail .= nl2br("Имя: \r\n" . $fio, false);
-    $msgEmail .= nl2br("Email: \r\n" . $email, false);
-    $msgEmail .= nl2br("Телефон: \r\n" . $phone, false);
-    $msgEmail .= nl2br("Дата: \r\n" . $date, false);
-    $msgEmail .= nl2br("Количество: \r\n" . $range, false);
-   
-    //проверка полей на валидацию
-    if (htmlspecialchars($fio) == ''){
-        $errors[] = 'Заполните поле : ФИО';
-    }
-    if (!preg_match("/^[a-zа-яё\d]{1}[a-zа-яё\d\s]*[a-zа-яё\d]{1}$/i", $fio) ) {
-        $errors[] = 'Поле ФИО не может содержать цифры или знаки';
-    }
-    if (mb_strlen($fio) < 2 ||  mb_strlen($fio) !== 20){
-        $errors[] = 'Ощибка: ФИО от 2 - х до 20 символов';
-    }
-    if (htmlspecialchars($phone) == ''){
-        $errors[] = 'Заполните поле : Номер телефона';
-    }
+//Сообщение на почту
+/*   $msgEmail = nl2br("Данные сообщения:\r\n", false);
+   $msgEmail .= nl2br("Email: \r\n" . $email, false);
+   $msgEmail .= nl2br("Телефон: \r\n" . $phone, false);
+   $msgEmail .= nl2br("Дата: \r\n" . $date, false);
+   $msgEmail .= nl2br("Количество: \r\n" . $range, false);*/
 
-    if(isset($_FILES)) {
-
-        $allowedTypes = array('.doc','.docx','.pdf');
-
-        $uploadDir = "php/"; //Директория загрузки. Если она не существует, обработчик не сможет загрузить файлы и выдаст ошибку
-
-        for($i = 0; $i < count($_FILES['file']['name']); $i++) { //Перебираем загруженные файлы
-
-        $uploadFile[$i] = $uploadDir . basename($_FILES['file']['name'][$i]);
-
-        $fileChecked[$i] = false;
-
-        echo $_FILES['file']['name'][$i]." | ".$_FILES['file']['type'][$i]." — ";
-
-        for($j = 0; $j < count($allowedTypes); $j++) { //Проверяем на соответствие допустимым форматам
-
-    if($_FILES['file']['type'][$i] == $allowedTypes[$j]) {
-
-        $fileChecked[$i] = true;
-
-        break;
-
-        }
-
-    }
-
-    if($fileChecked[$i]) { //Если формат допустим, перемещаем файл по указанному адресу
-
-    if(move_uploaded_file($_FILES['file']['tmp_name'][$i], $uploadFile[$i])) {
-
-        echo "Успешно загружен <br>";
-
-    } else {
-
-    echo "Ошибка ".$_FILES['file']['error'][$i]."<br>";
-
-    }   
-
-    } else {
-
-        echo "Недопустимый формат <br>";
-
-        }
-
-    }
-
-    } else {
-
-        echo "Вы не прислали файл!" ;
-
-    }
-
-    $Return = getCaptcha($_POST['g-recaptcha-response']);
-    
-    if($Return->success !== true && $Return->score < 0.5){
-       $errors[] = 'Ой! Кажется вы робот!';
-    }
-    
-    if ( $file > $_FILES['MAX_FILE_SIZE'] ){
-         $errors[] = 'Ошибка: Файл не более 5 мб';
+//проверка полей на валидацию
+/* if ($fio == '') {
+     $errors[] = 'Заполните поле : ФИО';
+ }elseif (!preg_match("/^[a-zа-яё\d]{1}[a-zа-яё\d\s]*[a-zа-яё\d]{1}$/i", $fio)) {
+     $errors[] = 'Поле ФИО не может содержать цифры или знаки';
+ }elseif ((mb_strlen($fio) > 20 || mb_strlen($fio) < 2)) {
+     $errors[] = 'Ощибка: ФИО от 2 - х до 20 символов';
+ }
+ if ($phone == '') {
+     $errors[] = 'Заполните поле : Номер телефона';
+ }
+ if (count($errors) > 0) {
+     foreach ($errors as $err) {
+         echo  json_encode($err, JSON_UNESCAPED_UNICODE);
      }
-    
-    
-    if ( $range < 20 ) {
-        $errors[] = 'Ошибка: Количество от 20 до 80';
-    }
-   if (count($errors) > 0) {
-        
-        $outErrors = '<div class="errors">';
-        foreach ($errors as $err) {
-          $outErrors .= $err.'<br>';
-        }
-        $outErrors .= '</div>';
-        echo $outErrors;
-        die;  
-    }
 
-}else {
-    $checkMail = mail('mityamazikin@gmail.com', 'Новое сообщение', $msgEmail);
-    $mysql = new mysqli('localhost','u0727235_admin','don@ld2899157','u0727235_gg');// установить кодировку
-    $mysql->query("SET NAMES 'utf-8'");
-    $mysql->query("INSERT INTO `form` (`date`, `email`, `fio`, `file`, `phone`, `inputrng`) VALUES('$date', '$email', '$fio', '$file', '$phone', '$range') "); //ошибка синтаксиса
-
-    $mysql->close();
-}
-
-
-
-
-//сделать уникальный Id таблицы 
-//запись должна идти при заполненных
-}else {
-    $checkMail = mail('mityamazikin@gmail.com', 'Новое сообщение', $msgEmail);
-    $mysql = new mysqli('localhost','u0727235_admin','don@ld2899157','u0727235_gg');// установить кодировку
-    $mysql->query("SET NAMES 'utf-8'");
-    $mysql->query("INSERT INTO `form` (`date`, `email`, `fio`, `file`, `phone`, `inputrng`) VALUES('$date', '$email', '$fio', '$file', '$phone', '$range') "); //ошибка синтаксиса
-
-    $mysql->close();
-}
-
-
+ }*/
 ?>

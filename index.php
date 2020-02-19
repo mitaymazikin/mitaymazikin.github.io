00b0c9c5009a17951a58a3cf74eec0cf8da73a9a
d.mazikin <mazikindima@mail.ru>
2020-02-19 16:01:27 +0300

    Переделываю форму

--- index.php
+++ index.php
@@ -1,49 +1,50 @@
 <!doctype html>
-<html lang="Ru" xmlns="http://www.w3.org/1999/html">
+<html lang="ru">
 <head>
     <!-- Required meta tags -->
-    <meta charset="<?php mb_internal_encoding("UTF-8"); ?>>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="<?php mb_internal_encoding("UTF-8"); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="/css/jquery-ui.css">
+    <link rel="stylesheet" href="/css/jquery-ui.css">
     <link rel="stylesheet" href="/css/style.css">
     <!-- Bootstrap CSS-->
     <title>Форма обратной связи</title>
 </head>
 <body>
-<div class="container">
+    <div class="container">
 
-    <form id="toForm" method="POST" name="check" enctype="multipart/form-data">
-        <h1 class="col-sm-12 text-center">Заполните форму, пожалуйста!</h1>
-        <div class="errors"></div>
-        <div class="form-group row">
-            <label  class="col-sm-2 col-form-label">Введите ФИО:</label>
-            <div class="col-sm-4">
-                <input name="fio" type="text" class="form-control" id="inputFio"  placeholder="Введите ФИО" >
+        <form id="toForm" method="POST" name="check" enctype="multipart/form-data">
+            <h1 class="col-sm-12 text-center">Заполните форму, пожалуйста!</h1>
+            <div class="errors"></div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Введите ФИО:</label>
+                <div class="col-sm-4">
+                    <input name="fio" type="text" class="form-control" id="inputFio" placeholder="Введите ФИО">
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Номер телефона</label>
-            <div class="col-sm-4">
-                <input name="phone" type="tel" class="form-control" id="inputPhone" >
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Номер телефона</label>
+                <div class="col-sm-4">
+                    <input name="phone" type="tel" class="form-control" id="inputPhone"
+                           placeholder="+7 (999) 99 99 999">
+                </div>
             </div>
-        </div>
 
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Email</label>
-            <div class="col-sm-4">
-                <input name="email" type="email" class="form-control" id="inpuEmail" placeholder="Email">
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                    <input name="email" type="email" class="form-control" id="inpuEmail" placeholder="example@mail.ru">
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label >Загрузите файл:</label>
-            <!-- <input type="hidden" name="MAX_FILE_SIZE" value="50000" /> -->
-            <input name="file" type="file" class="form-control-file" id="files">
-        </div>
-        <div class="form-group row">
-            <label  class="col-sm-2 col-form-label">Дата:</label>
-            <div class="col-sm-4">
+            <div class="form-group">
+                <label>Загрузите файл:</label>
+                <!-- <input type="hidden" name="MAX_FILE_SIZE" value="50000" /> -->
+                <input name="file" type="file" class="form-control-file" id="files">
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Дата:</label>
+                <div class="col-sm-4">
                 <input name="date" type="text" class="form-control" id="inputDate">
             </div>
         </div>
@@ -55,19 +56,17 @@
                 </div>
             </div>
         </div>
-        <div class="form-group row">
-            <div class="col-sm-12 ">
-                <button type="submit" id="submit" name="check_submit" class="btn btn-success">Отправить</button>
+            <div class="form-group row">
+                <div class="col-sm-12 ">
+                    <button type="submit" id="submit" name="check_submit" class="btn btn-success">Отправить</button>
+                </div>
             </div>
-        </div>
-    </form>
-
-</div>
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script type="text/javascript" src="/js/jquery-3.4.1.min.js"></script>
-<script type="text/javascript" src="/js/jquery.maskedinput.min.js"></script>
-<script type="text/javascript" src="/js/jquery-ui.js"></script>
-<script src="/js/main.js"></script>
-<script src="/js/filecheck.js"></script>
+        </form>
+    </div>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="/js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="/js/jquery.maskedinput.js"></script>
+    <script type="text/javascript" src="/js/jquery-ui.js"></script>
+    <script type="text/javascript" src="/js/main.js"></script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-    $("#inputPhone").mask("+7(999) 999-99-99");
-    //начало календаря
     $( "#inputDate" ).datepicker({
         monthNames: ['Январь', 'Февраль', 'Март', 'Апрель',
             'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь',
@@ -8,73 +6,21 @@ $(document).ready(function(){
         dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
         firstDay: 1,
     });
-    //конец календаря
 
-    //ползунок
-    $( function() {
-        var handle = $( "#custom-handle" );
-        $( "#slider" ).slider({
-            create: function() {
-                handle.text( $( this ).slider( "value" ) );
-            },
-            slide: function( event, ui ) {
-                handle.text( ui.value );
-            }
-        });
-    } );
+    $("#inputPhone").mask("+7 (999) 99-99-999");
 
-    //проверка ошибок
-    /*$('#toForm').submit(function(e){
-        var newform = new FormData( document.querySelector);
-        $.ajax({
-            url: 'php/check.php',
-            data: newform,
-            cache: false,
-            processData: false,
-            contentType: false,
-            type: 'POST',
-            dataType: 'JSON',
-            success: function (data) {
-                console.log(newform);
-            }
-        });
-
-    });*/
-
-
+    //Обработка данных с сервера
     toForm.onsubmit = async (e) => {
         e.preventDefault();
 
         let response = await fetch('/php/check.php', {
             method: 'POST',
-            body: new FormData(toForm)
+            body:   new FormData(toForm)
         });
 
         let result = await response.json();
 
         console.log(result);
     };
-
-
-    /*$("#toForm").submit(function(){
-      var dataCallback = $(this).serialize();
-
-      //console.log(dataCallback);
-      console.log(dataCallback);
-
-      var action = $(this).attr('action');
-      if (dataCallback && action) { //проверка не до конца корректна
-          $.ajax({
-            type: "POST",
-            url: action,
-            data: dataCallback,
-            success: function(out){
-              console.log(out);
-            }
-          });
-        };
-        return false;
-      });*/
-
-
+    //Конец обработки данных
 });
